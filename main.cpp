357566dbfefd3e675aeceafd2a9e713ba2208364
bipy <notbipy@gmail.com>
2020-07-07 13:06:58 +0800

    fix BC algo & update user interface

--- main.cpp
+++ main.cpp
@@ -16,9 +16,11 @@ struct node {
 unordered_set<string> ref_set;
 unordered_map<string, int> trans;
 unordered_map<int, string> reverse_trans;
-vector<vector<int> > matrix, path;
+vector<vector<int> > matrix;
 unordered_map<string, unordered_map<string, bool> > m;
 vector<node> ans;
+vector<int> path_count;
+vector<string> methods;
 
 string ref_data, input_data;
 
@@ -30,7 +32,7 @@ bool cmp(const node &a, const node &b);
 
 void output(vector<node> &cur, vector<double> rate, const string &method);
 
-vector<double> compare_ref(const int step);
+vector<double> compare_ref(int step);
 
 void DC();
 
@@ -43,7 +45,6 @@ void check(const string &method);
 void help();
 
 int main(int argc, char **argv) {
-    string method;
     if (argc == 1) {
         help();
         system("pause");
@@ -55,11 +56,11 @@ int main(int argc, char **argv) {
             help();
             return 0;
         } else if (cur == "-d") {
-            method = "DC";
+            methods.emplace_back("DC");
         } else if (cur == "-c") {
-            method = "CC";
+            methods.emplace_back("CC");
         } else if (cur == "-b") {
-            method = "BC";
+            methods.emplace_back("BC");
         } else if (cur == "-r") {
             if (i + 1 < argc) {
                 ref_data = argv[i + 1];
@@ -72,13 +73,37 @@ int main(int argc, char **argv) {
             }
         }
     }
-    if (input_data.empty() || ref_data.empty() || method.empty()) {
+    if (input_data.empty() || ref_data.empty() || methods.empty()) {
         printf("Invalid args!\nUse -h for help.");
         return 1;
     }
     init();
-    check(method);
+    sort(methods.begin(), methods.end());
+    for (auto &it:methods) {
+        check(it);
+    }
     return 0;
+}
+
+void help() {
+    printf("===== Center Protein Calc =====\n\n");
+    printf("Usage: [option] [value]\n");
+    printf("-h See this.\n");
+    printf("-i Specific input data path.\n");
+    printf("-r Specific reference data path.\n");
+    printf("-d Use algorithm DC.\n");
+    printf("-c Use algorithm CC.\n");
+    printf("-b Use algorithm BC.\n\n");
+    printf("=========== Caution ===========\n\n");
+    printf("Must have at least one of '-b', '-c' or '-d'.\n");
+    printf("Must have '-r' and 'your refer data's path'.\n");
+    printf("Must have '-i' and  'your input data's path'.\n");
+    printf("Algorithm BC or CC will take a long trip to run (like O(N^3)), but it works!\n\n");
+    printf("============ Tips ============\n\n");
+    printf("Use '-b -c' together (save 50%% time)\n\n");
+    printf("============ About ============\n\n");
+    printf("Author: bipy@GitHub\n");
+    printf("Version: 20200707.2\n\n");
 }
 
 bool cmp(const node &a, const node &b) {
@@ -86,32 +111,44 @@ bool cmp(const node &a, const node &b) {
 }
 
 void init() {
-    ifstream fin(input_data, ios::in);
-    if (!fin) {
+    // init input
+    ifstream data_in(input_data, ios::in);
+    if (!data_in) {
         cout << input_data + " NOT FOUND!" << endl;
         return;
     }
-    string node_a, node_b;
-    getline(fin, node_a);
+    string node_a, node_b, s;
+    getline(data_in, node_a);
     if (!regex_match(node_a, regex("^node.*$"))) {
-        fin.seekg(ios::beg);
+        data_in.seekg(ios::beg);
     }
-    while (getline(fin, node_a, '\t') && getline(fin, node_b)) {
+    while (getline(data_in, node_a, '\t') && getline(data_in, node_b)) {
         m[node_a][node_b] = m[node_b][node_a] = true;
     }
-    fin.close();
+    data_in.close();
+    // init reference
+    ifstream ref_in(ref_data, ios::in);
+    if (!ref_in) {
+        cout << ref_data + " NOT FOUND!" << endl;
+        return;
+    }
+    while (getline(ref_in, s)) {
+        ref_set.insert(s);
+    }
+    ref_in.close();
 }
 
 void trans_matrix() {
     matrix.resize(m.size(), vector<int>(m.size(), INT16_MAX));
     int index = 0;
-    for (auto it = m.begin(); it != m.end(); it++, index++) {
-        trans[it->first] = index;
-        reverse_trans[index] = it->first;
+    for (auto &it : m) {
+        trans[it.first] = index;
+        reverse_trans[index] = it.first;
+        index++;
     }
-    for (auto it = m.begin(); it != m.end(); it++) {
-        for (auto i = it->second.begin(); i != it->second.end(); i++) {
-            int a = trans[it->first], b = trans[i->first];
+    for (auto &it : m) {
+        for (auto &i:it.second) {
+            int a = trans[it.first], b = trans[i.first];
             matrix[a][b] = 1;
         }
     }
@@ -119,27 +156,19 @@ void trans_matrix() {
 
 void output(vector<node> &cur, vector<double> rate, const string &method) {
     string dest = input_data.substr(0, input_data.size() - 4) + " output_" + method + ".txt";
-    ofstream fout(dest, ios::out);
+    ofstream ans_out(dest, ios::out);
     for (int i = 0; i < rate.size(); i++) {
-        fout << (i + 1) * 100 << " " << rate[i] << endl;
+        ans_out << (i + 1) * 100 << " " << rate[i] << endl;
     }
-    for (auto it = cur.begin(); it != cur.end(); it++) {
-        fout << it->id << endl;
+    ans_out << endl;
+    for (auto &it : cur) {
+        ans_out << it.id << endl;
     }
-    fout.close();
+    ans_out.close();
 }
 
 vector<double> compare_ref(const int step) {
     vector<double> rt;
-    ifstream fin(ref_data, ios::in);
-    if (!fin) {
-        cout << ref_data + " NOT FOUND!" << endl;
-        return rt;
-    }
-    string s;
-    while (getline(fin, s)) {
-        ref_set.insert(s);
-    }
     int count = 0;
     for (int i = 0; i < ans.size(); i++) {
         if (ref_set.find(ans[i].id) != ref_set.end()) {
@@ -153,6 +182,7 @@ vector<double> compare_ref(const int step) {
 }
 
 void check(const string &method) {
+    cout << method + " start!" << endl;
     if (method == "DC") {
         DC();
     } else if (method == "CC") {
@@ -162,35 +192,25 @@ void check(const string &method) {
     }
     sort(ans.begin(), ans.end(), cmp);
     output(ans, compare_ref(100), method);
-}
-
-void help() {
-    printf("==== Center Protein Calc ====\n");
-    printf("Usage: [option] [value]\n");
-    printf("-h See this.\n");
-    printf("-i Specific input data path.\n");
-    printf("-r Specific reference data path.\n");
-    printf("-d Use algorithm DC.\n");
-    printf("-c Use algorithm CC.\n");
-    printf("-b Use algorithm BC.\n");
-    printf("=========================\n");
-    printf("Author: bipy@GitHub\n");
-    printf("Version: 20200707.1\n");
+    cout << method + " complete!" << endl;
 }
 
 void floyd(bool count_path) {
     trans_matrix();
     int size = matrix.size();
     if (count_path) {
-        path.resize(size, vector<int>(size, 0));
+        path_count.resize(size, 0);
     }
     for (int k = 0; k < size; k++) {
+        if (k % 100 == 0) {
+            printf("Progress: %.1f %%\n", 100 * static_cast<double>(k) / size);
+        }
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {
                     matrix[i][j] = matrix[i][k] + matrix[k][j];
                     if (count_path) {
-                        path[i][j] = k;
+                        path_count[k]++;
                     }
                 }
             }
@@ -199,13 +219,15 @@ void floyd(bool count_path) {
 }
 
 void DC() {
-    for (auto it = m.begin(); it != m.end(); it++) {
-        ans.push_back(node{it->first, static_cast<double>(it->second.size())});
+    for (auto &it : m) {
+        ans.push_back(node{it.first, static_cast<double>(it.second.size())});
     }
 }
 
 void CC() {
-    floyd(false);
+    if (path_count.empty()) {
+        floyd(false);
+    }
     int size = matrix.size();
     for (int i = 0; i < size; i++) {
         int sum = 0;
@@ -218,14 +240,8 @@ void CC() {
 
 void BC() {
     floyd(true);
-    int size = matrix.size();
-    vector<int> pass_count(size, 0);
+    int size = path_count.size();
     for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            pass_count[path[i][j]]++;
-        }
-    }
-    for (int i = 0; i < size; i++) {
-        ans.push_back(node{reverse_trans[i], static_cast<double>(pass_count[i])});
+        ans.push_back(node{reverse_trans[i], static_cast<double>(path_count[i])});
     }
 }
