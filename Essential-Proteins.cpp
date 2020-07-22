248401e83ab23e3b003e7d6e4ee9c987efbcc462
bipy <notbipy@gmail.com>
2020-07-23 01:46:10 +0800

    add SigEP

--- Essential-Proteins.cpp
+++ Essential-Proteins.cpp
@@ -8,18 +8,13 @@
 #include <fstream>
 #include <regex>
 #include <cctype>
-#include <gmp.h>
+#include <cmath>
 
 using namespace std;
 // data unit
 struct node {
     string id;
     double val;
-};
-// gmp mpq data unit
-struct mpq_node {
-    string id;
-    mpq_t val;
 };
 
 // reference data (const)
@@ -33,6 +28,8 @@ vector<vector<int> > matrix, path;
 unordered_map<string, unordered_map<string, bool> > m;
 // Answer sheet (clear each time)
 vector<node> ans;
+// Log(factorial) record
+vector<double> log_fac;
 
 vector<int> path_count;
 set<string> methods;
@@ -42,15 +39,15 @@ string ref_data, input_data;
 int step = 100;
 
 // check and input data
-void init();
+bool init();
 
 // trans list to matrix
 void trans_matrix(int initial_val);
 
 // comparator
-bool cmp(const node &a, const node &b);
+bool cmp_less(const node &a, const node &b);
 
-bool mpq_unit_cmp(const mpq_node &a, const mpq_node &b);
+bool cmp_greater(const node &a, const node &b);
 
 // output results
 // vector<node> &cur: sorted data
@@ -64,16 +61,17 @@ vector<int> compare_ref();
 // BC: count pass through between "start" and "end"
 void get_path_count(const int &start, const int &end);
 
-// get mpz_t nu&de of a Combination Number
-// nCr(b,a)
-void mpz_comb_nu_de_ui(mpz_ptr nu, mpz_ptr de, unsigned int a, unsigned int b);
+// return log(x!)
+double get_log_fac(unsigned int x);
 
-// get result of a Combination Number
-// nCr(b,a)
-void mpz_comb_ui(mpz_ptr cur, unsigned int a, unsigned int b);
+// return log(nCr(N,r))
+double get_log_comb(unsigned int N, unsigned int r);
 
-// Algorithm: Degree Centrality with p-value
-void DC_P();
+// return nCr(N,r)
+unsigned int get_exact_comb(unsigned int N, unsigned int r);
+
+// Algorithm: Significance-Based Essential Protein Discovery
+void SigEP();
 
 // Algorithm: Eigenvector Centrality
 void EC();
@@ -105,7 +103,7 @@ int main(int argc, char **argv) {
             help();
             return 0;
         } else if (cur == "-p") {
-            methods.insert("DC_P");
+            methods.insert("SigEP");
         } else if (cur == "-e") {
             methods.insert("EC");
         } else if (cur == "-d") {
@@ -146,11 +144,13 @@ int main(int argc, char **argv) {
     }
     if (input_data.empty() || ref_data.empty() || methods.empty()) {
         printf("Invalid args!\nUse -h for help.");
-        return 1;
+        return 2;
     }
-    init();
+    if (!init()) {
+        return 3;
+    }
     if (*methods.begin() == "ALL") {
-        methods = {"BC", "CC", "DC", "EC", "DC_P"};
+        methods = {"BC", "CC", "DC", "EC", "SigEP"};
     }
     for (auto &it : methods) {
         check(it);
@@ -165,12 +165,12 @@ void help() {
     printf("-i Specific input data path.\n");
     printf("-r Specific reference data path.\n");
     printf("-s (optional) Specific step (default 100).\n");
-    printf("-a Use 5 centrality algorithms together (BC, CC, DC, EC, DC_P).\n");
+    printf("-a Use 5 centrality algorithms together (BC, CC, DC, EC, SigEP).\n");
     printf("-b Use algorithm Betweenness Centrality (BC).\n");
     printf("-c Use algorithm Closeness Centrality (CC).\n");
     printf("-d Use algorithm Degree Centrality (DC).\n");
     printf("-e Use algorithm Eigenvector Centrality (EC).\n");
-    printf("-p Use algorithm Degree Centrality with p-value (DC_P).\n\n");
+    printf("-p Use algorithm Significance-Based Essential Protein Discovery (SigEP).\n\n");
     printf("=========== Caution ===========\n\n");
     printf("Must have at least one of ['-a', '-b', '-c', '-d', '-e', '-p'].\n");
     printf("Must have '-r' and 'your refer data's path'.\n");
@@ -180,23 +180,24 @@ void help() {
     printf("Use '-b -c' together (save you 50%% time)\n\n");
     printf("============ About ============\n\n");
     printf("Author: bipy@GitHub\n");
-    printf("Version: 20200721.1\n\n");
+    printf("Version: 20200722.1\n\n");
 }
 
-bool cmp(const node &a, const node &b) {
+bool cmp_less(const node &a, const node &b) {
+    return a.val < b.val;
+}
+
+bool cmp_greater(const node &a, const node &b) {
     return a.val > b.val;
 }
 
-bool mpq_unit_cmp(const mpq_node &a, const mpq_node &b) {
-    return mpq_cmp(a.val, b.val) < 0;
-}
 
-void init() {
+bool init() {
     // init input
     ifstream data_in(input_data, ios::in);
     if (!data_in) {
         cout << input_data + " NOT FOUND!" << endl;
-        return;
+        return false;
     }
     string node_a, node_b, s;
     getline(data_in, node_a);
@@ -211,12 +212,13 @@ void init() {
     ifstream ref_in(ref_data, ios::in);
     if (!ref_in) {
         cout << ref_data + " NOT FOUND!" << endl;
-        return;
+        return false;
     }
     while (getline(ref_in, s)) {
         ref_set.insert(s);
     }
     ref_in.close();
+    return true;
 }
 
 void trans_matrix(int initial_val) {
@@ -285,10 +287,9 @@ void check(const string &method) {
         BC();
     } else if (method == "EC") {
         EC();
-    } else if (method == "DC_P") {
-        DC_P();
+    } else if (method == "SigEP") {
+        SigEP();
     }
-    sort(ans.begin(), ans.end(), cmp);
     output(ans, compare_ref(), method);
     ans.clear();
     printf("%s complete!\n\n", method.c_str());
@@ -318,44 +319,25 @@ void floyd(bool count_path) {
     }
 }
 
-void mpz_comb_nu_de_ui(mpz_ptr nu, mpz_ptr de, unsigned int a, unsigned int b) {
-    mpz_init_set_ui(nu, b);
-    mpz_init_set_ui(de, a);
-    for (unsigned int i = 1; i < a; i++) {
-        mpz_mul_ui(nu, nu, i + b - a);
-        mpz_mul_ui(de, de, i);
+double get_log_fac(unsigned int x) {
+    for (unsigned int i = log_fac.size(); i <= x; i++) {
+        log_fac.push_back(log_fac.back());
+        log_fac[i] += log2(i);
     }
+    return log_fac[x];
 }
 
-void mpz_comb_ui(mpz_ptr cur, unsigned int a, unsigned int b) {
-    if (a > b) {
-        mpz_comb_ui(cur, b, a);
-        return;
+double get_log_comb(unsigned int N, unsigned int r) {
+    if (N < r) {
+        return get_log_comb(r, N);
     }
-    mpz_t de, nu;
-    mpz_comb_nu_de_ui(nu, de, a, b);
-    mpz_init(cur);
-    mpz_div(cur, nu, de);
+    return get_log_fac(N) - get_log_fac(r) - get_log_fac(N - r);
 }
 
-// unused method
-//void mpz_comb(mpz_t &ptr, mpz_t &a, mpz_t &b) {
-//    if (mpz_cmp(a, b) > 0) {
-//        mpz_comb(ptr, b, a);
-//        return;
-//    }
-//    mpz_t de, nu, i, b_a;
-//    mpz_init_set(nu, b);
-//    mpz_init_set(de, a);
-//    mpz_sub(b_a, b, a);
-//    for (mpz_init_set_ui(i, 2); mpz_cmp(i, a) < 0; mpz_add_ui(i, i, 1)) {
-//        mpz_mul(de, de, i);
-//    }
-//    for (mpz_add_ui(i, b_a, 1); mpz_cmp(i, b) < 0; mpz_add_ui(i, i, 1)) {
-//        mpz_mul(nu, nu, i);
-//    }
-//    mpz_div(ptr, nu, de);
-//}
+unsigned int get_exact_comb(unsigned int N, unsigned int r) {
+    return lround(exp2(get_log_comb(N, r)));
+}
+
 
 void EC() {
     trans_matrix(-1);
@@ -370,6 +352,8 @@ void EC() {
         // push into ans
         ans.emplace_back(node{reverse_trans[i], l});
     }
+    // descending sort
+    sort(ans.begin(), ans.end(), cmp_greater);
 }
 
 void DC() {
@@ -377,10 +361,12 @@ void DC() {
     for (auto &it : m) {
         ans.emplace_back(node{it.first, static_cast<double>(it.second.size())});
     }
+    // descending sort
+    sort(ans.begin(), ans.end(), cmp_greater);
 }
 
 void CC() {
-    // set ensures Algo:BC is ahead of Algo:CC
+    // STL "set" ensures Algo:BC is ahead of Algo:CC
     // if floyd has processed before, skip
     if (path.empty()) {
         floyd(false);
@@ -391,9 +377,11 @@ void CC() {
         for (int j = 0; j < size; j++) {
             sum += matrix[i][j];
         }
-        // use multiplicative inverse * 10,000 as value push into ans
-        ans.emplace_back(node{reverse_trans[i], 10000.0 / sum});
+        // use distance sum as value push into ans
+        ans.emplace_back(node{reverse_trans[i], static_cast<double>(sum)});
     }
+    // ascending sort
+    sort(ans.begin(), ans.end(), cmp_less);
 }
 
 void BC() {
@@ -414,104 +402,79 @@ void BC() {
     for (int i = 0; i < size; i++) {
         ans.emplace_back(node{reverse_trans[i], static_cast<double>(path_count[i])});
     }
+    // descending sort
+    sort(ans.begin(), ans.end(), cmp_greater);
 }
 
-void DC_P() {
+void SigEP() {
     /**
      *  To ensure C_N_2 is less than 0xffffffffU
      *  N must be less than about 92,000
-     *  requirement: libgmp 6.20
      */
 
     // N: the number of vertexes
     // M: the number of edges
     unsigned int N = m.size(), M = 0;
+
     // calc M
     for (auto &it : m) {
         M += it.second.size();
     }
     M /= 2;
-    // limit: min(N - 1, M) (> 0)
-    unsigned int limit = min(N - 1, M);
+
+    // init log_fac: log(0!) = 0.0
+    log_fac.push_back(0.0);
+
     // C_N_2: nCr(N,2)
-    // de: denominator (pi)
-    mpz_t C_N_2, de;
-    mpz_comb_ui(C_N_2, 2, N);
-    mpz_comb_ui(de, M, mpz_get_ui(C_N_2));
-    // dp array
-    vector<mpz_t> mpz_nu(limit + 1);
-    // sorted list
-    vector<mpq_node> mpq_v(N);
-    // All about pi's numerator
-    mpz_t de_0, nu_0, temp_0, de_1, nu_1, temp_1;
-    mpz_init_set_ui(nu_0, 1);
-    mpz_init_set_ui(de_0, 1);
+    unsigned int C_N_2 = get_exact_comb(N, 2);
 
-    // foreach d: Calc pd O(N)
-    for (unsigned int d = 0; d <= limit; d++) {
-        // progress interface
-        if (d % 100 == 0) {
-            printf("p-value: %.1f %%\n", 100 * static_cast<double>(d) / limit);
-        }
-        // Let nCr(N,0) and nCr(N,N) be 1
-        if (d != 0 && d != limit) {
-            // trans to next form
-            mpz_mul_ui(nu_0, nu_0, (N - 1) - d + 1);
-            mpz_mul_ui(de_0, de_0, d);
-            // (pi's numerator)'s first part
-            mpz_divexact(temp_0, nu_0, de_0);
-            // init (pi's numerator)'s second part
-            if (d == 1) {
-                mpz_comb_nu_de_ui(nu_1, de_1, M - 1, mpz_get_ui(C_N_2) - (N - 1));
-            } else {
-                // trans to next form
-                // use multiplicative inverse instead of divsion
-                mpz_mul_ui(nu_1, nu_1, M - d + 1);
-                mpz_mul_ui(de_1, de_1, mpz_get_ui(C_N_2) - (N - 1) - (M - d));
-            }
-            mpz_divexact(temp_1, nu_1, de_1);
-        } else {
-            mpz_init_set_ui(temp_0, 1);
-            mpz_init_set_ui(temp_1, 1);
-        }
-        // put first part cross second part into array
-        mpz_init(mpz_nu[d]);
-        mpz_mul(mpz_nu[d], temp_0, temp_1);
-        // recursion
-        if (d > 0) {
-            mpz_add(mpz_nu[d], mpz_nu[d], mpz_nu[d - 1]);
-        }
-    }
+    // de: log(denominator (pi))
+    double de = get_log_comb(C_N_2, M);
 
-    // foreach vertex i, calculate pi
-    // init
-    mpz_t pi_nu;
-    mpz_init(pi_nu);
-    mpq_t pi, pi_f_nu, pi_f_de;
-    mpq_inits(pi, pi_f_nu, pi_f_de, NULL);
-    mpq_set_z(pi_f_de, de);
-
-    // calculate pi
-    unsigned int i = 0;
+    // foreach vertex
     for (auto &it : m) {
-        mpq_v[i].id = it.first;
-        mpq_init(mpq_v[i].val);
-        int d = it.second.size();
-        if (d == 0) {
-            mpz_set(pi_nu, mpz_nu.back());
-        } else {
-            mpz_sub(pi_nu, mpz_nu.back(), mpz_nu[d - 1]);
+        // p: log(pi)
+        double p = 0.0;
+        // e: neighbour's edges
+        // cur_d: current vertex's degree
+        int e = 0, cur_d = it.second.size();
+        // count e
+        for (auto i = it.second.begin(); i != it.second.end(); i++) {
+            for (auto j = i; j != it.second.end(); j++) {
+                if (i == j) continue;
+                if (m[i->first].find(j->first) != m[i->first].end()) {
+                    e++;
+                }
+            }
         }
-        // trans to mpq_t
-        mpq_set_z(pi_f_nu, pi_nu);
-        mpq_div(mpq_v[i].val, pi_f_nu, pi_f_de);
-        i++;
+        // when d == 1, specific c = 0, then ignore est
+        // c: local clustering coefficient
+        double c = cur_d == 1 ? 0.0 : e / static_cast<double>(cur_d * (cur_d - 1));
+        // est: the third arg of min()
+        unsigned int est = c == 0.0 ? M : static_cast<unsigned int>(
+                floor((c - 2 + sqrt(pow(2 - c, 2) + 8 * c * M)) / (2 * c))
+        );
+        // beta: the upper limit of degree
+        unsigned int beta = min(min(est, M), N - 1);
+
+        // foreach degree: from current vertex's degree to beta's
+        for (unsigned int d = cur_d; d < beta; d++) {
+            // C_d_2: nCr(d,2)
+            unsigned int C_d_2 = get_exact_comb(d, 2);
+            // cc: c cross C_d_2
+            auto cc = static_cast<unsigned int>(ceil(c * C_d_2));
+            // nu_0: the first term of numerator
+            double nu_0 = get_log_comb(N - 1, d);
+            // nu_1: the second term of numerator
+            double nu_1 = get_log_comb(C_d_2, cc);
+            // nu_2: the third term of numerator
+            double nu_2 = get_log_comb(C_N_2 - (N - 1) - cc, M - d - cc);
+
+            p += nu_0 + nu_1 + nu_2;
+        }
+        p += log(N) - de;
+        ans.emplace_back(node{it.first, p});
     }
-    // ascending sort by pi
-    sort(mpq_v.begin(), mpq_v.end(), mpq_unit_cmp);
-    // push into ans according to the previous sort result
-    double index = N + 1.0;
-    for (auto &it : mpq_v) {
-        ans.emplace_back(node{it.id, index--});
-    }
+    // ascending sort
+    sort(ans.begin(), ans.end(), cmp_less);
 }
